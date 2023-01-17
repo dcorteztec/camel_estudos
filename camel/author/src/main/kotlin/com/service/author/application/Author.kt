e859896df9af0c8db481c56e04cdb354422ab5ef
dcorteztec@gmail.com <dcorteztec@gmail.com>
2023-01-16 22:23:15 -0300

    ajuste commit

--- camel/author/src/main/kotlin/com/service/author/application/Author.kt
+++
@@ -1,9 +0,0 @@
-package com.service.author.application
-
-data class Author(
-    var id:String,
-
-    var name:String,
-
-    var fullName:String
-)
--- camel/book/src/main/kotlin/com/service/book/application/Book.kt
+++
@@ -1,9 +0,0 @@
-package com.service.book.application
-
-data class Book(
-    var title: String,
-
-    var description: String,
-
-    var authorId: String
-)
